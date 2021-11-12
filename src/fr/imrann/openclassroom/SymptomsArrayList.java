0409aeff3aae67a95d399995d5e914af8546589d
Imrann John <life.imrann@gmail.com>
2021-11-12 10:47:11 +0100

    ArrayList fonctionnel sans trie

--- src/fr/imrann/openclassroom/SymptomsArrayList.java
+++ src/fr/imrann/openclassroom/SymptomsArrayList.java
@@ -3,7 +3,7 @@ package fr.imrann.openclassroom;
 import java.util.ArrayList;
 
 public interface SymptomsArrayList  {
-	
-	public ArrayList<String> symptomsList();
+
+	ArrayList<String> SymptomsList();
 
 }
--- src/fr/imrann/openclassroom/SymptomsReader.java
+++ src/fr/imrann/openclassroom/SymptomsReader.java
@@ -1,63 +1,55 @@
 package fr.imrann.openclassroom;
 
-import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-
-public class SymptomsReader implements SymptomsArrayList {
-	
-	
-	
-	BufferedReader symptomsReader = new BufferedReader(new FileReader("/Users/imrannjohn/Documents/Workspace-eclipse/Projet_Openclassroom/src/fr/imrann/openclassroom/symptoms.txt"));
-	String line = symptomsReader.readLine();
-	
-	
-	
-
+public class SymptomsReader {
+											
+	public ArrayList<String> symptomsList(String filepath) {
+		ArrayList<String> symptomsList = new ArrayList<String>();
 		
-
-	public ArrayList<String> symptomsList() {
 		
-			ArrayList<String> symptomsList = new ArrayList<String>();	
-				
-				try {
-					
-					
-					
-				
-						if (line != null) {
-							
-							symptomsList.add(line);
-							line = symptomsReader.readLine();
-							System.out.println(symptomsList());
-
-							
-						}
-				}
-				
-				catch (IOException e) {
+			if(filepath != null ) {
+					try {
+							Scanner scnSymptoms = new Scanner (new FileReader(filepath));
+								
+								while(scnSymptoms.hasNextLine()) {
+									String ln = scnSymptoms.nextLine();
+									System.out.println(ln);
+									symptomsList.add(ln);
+									
+								}
+								
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					System.out.println("Fichier non trouvé!");
 					e.printStackTrace();
 				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			
-					return symptomsList();
-				
-	}
-
-	public static void main(String args[]){
+					finally {
+						System.out.println("Fini!");
+					}					
+			}
+			return symptomsList;
+ }
+	public static void main(String[] args) {
 		SymptomsReader sr = new SymptomsReader();
-
+		System.out.println(sr.symptomsList("/Users/imrannjohn/Documents/Workspace-eclipse/Projet_Openclassroom/src/fr/imrann/openclassroom/symptoms.txt"));
+		
 	}
-	
 }
 
+	
 
 	
+
+
+
+
+
 	
+
 	
 	
 
